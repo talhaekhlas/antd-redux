13fc5a5cbf26dd41515fa34920962298ecea8fda
Talha Ekhlas <ekhlas.cse08@gmail.com>
2019-10-27 22:27:57 +0600

    Module List Start

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -17,8 +17,8 @@ export default class Root extends Component {
 
         <Router>
           
-          {/* <Layouts/> */}
-          <TestLayout/>
+          <Layouts/>
+          {/* <TestLayout/> */}
 
          
 
