0bf787b4b27e61aaa828c7be6c47027a5f9f084c
Talha Ekhlas <ekhlas.cse08@gmail.com>
2019-10-31 07:30:28 +0600

    todo list inner menu

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -13,22 +13,28 @@ class TodoList extends Component {
 
     todoOnMouseEnter = (e)=>{
         
-        console.log(e.target)
+        console.log('enter',e.target)
 
         //voutik achoron kore children a without this code
         if(e.target.children.length>1)
         {
             e.target.children[1].className = 'todo-mouse-enter'
+        }else{
+            console.log('enter parent',e.target.parentNode)
+            e.target.parentNode.children[1].className = 'todo-mouse-enter'
         }
         
     } 
     
     todoOnMouseLeave = (e)=>{
-        console.log(e.target)
+        console.log('Leave',e.target)
          //voutik achoron kore children a without this code
         if(e.target.children.length>1)
         {
             e.target.children[1].className = 'todo-mouse-leave'
+        }else{
+            console.log('leave parent',e.target.parentNode)
+            e.target.parentNode.children[1].className = 'todo-mouse-leave'
         }
         
     } 
@@ -52,8 +58,22 @@ class TodoList extends Component {
                     onMouseLeave={this.todoOnMouseLeave}
                     >
                         <span className="todo-single-name">{item.name}</span> 
-                        <span className='todo-mouse-leave'>sdfsdf</span>
-                        <span className="todo-delete-button">delete</span>
+                        <span className='todo-mouse-leave'>
+                            <span className="todo-mouse-enter-action-menu">
+                                <span style={{marginLeft:'20px',fontSize:'12px',color:'#7acef4'}}>Family</span>
+                                <span style={{marginLeft:'20px',fontSize:'17px',color:'#7acef4'}}>|</span>
+                                <span style={{marginLeft:'20px',fontSize:'12px',color:'#7acef4'}}>06:25 PM</span>
+                                <span style={{marginLeft:'20px',fontSize:'17px',color:'#d2d9e7'}}>|</span>
+                                <span style={{marginLeft:'20px',fontSize:'17px',color:'#d2d9e7'}}><i className="fa fa-pencil-square-o" aria-hidden="true"></i></span>
+                                <span style={{marginLeft:'20px',fontSize:'17px',color:'#d2d9e7'}}>|</span>
+                                <span style={{marginLeft:'20px',fontSize:'17px',color:'#d2d9e7'}}><i className="fa fa-paperclip" aria-hidden="true"></i></span>
+                                <span style={{marginLeft:'20px',fontSize:'17px',color:'#d2d9e7'}}>|</span>
+                                <span style={{marginLeft:'20px',fontSize:'17px',color:'#ebc84a'}}><i className="fa fa-star-o" aria-hidden="true"></i></span>
+                            </span>
+                            <span className="todo-delete-button" style={{color:'white',fontSize:'20px'}}><i className="fa fa-trash-o" aria-hidden="true"></i></span>
+                            
+                        </span>
+                        
                     </li>)}
                 </ul>
                
